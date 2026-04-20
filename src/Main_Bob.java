f8a464bdf97b8eeefa4d4d8f55e111bd32c529ef
marcin-kmp <mduda.kmp@gmail.com>
2026-04-20 19:49:24 +0200

    Add 'alice-github' print statement
    Added a new print statement for 'alice-github'.

--- src/Main_Bob.java
+++ src/Main_Bob.java
@@ -6,5 +6,6 @@ public class Main_Bob {
         System.out.println("alice-f");
         System.out.println("alice-g");
         System.out.println("alice-h");
+        System.out.println("alice-github");
     }
 }
