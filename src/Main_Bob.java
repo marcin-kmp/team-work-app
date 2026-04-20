2a40129873cac5628aaa2f2a76187e3b415b7003
marcin-kmp <mduda.kmp@gmail.com>
2026-04-20 18:23:59 +0200

    alice-e

--- src/Main_Bob.java
+++ src/Main_Bob.java
@@ -2,5 +2,6 @@ public class Main_Bob {
     public static void main(String[] args) {
         System.out.println("bob-b");
         System.out.println("alice-d");
+        System.out.println("alice-e");
     }
 }
