c7f6c8baf45b2652f02ecf0c5351fdc1bc6926b4
marcin-kmp <mduda.kmp@gmail.com>
2026-04-20 17:41:47 +0200

    bob-b

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("alice-a");
         System.out.println("alice-c");
+        System.out.println("bob-b");
     }
 }
---
+++ src/Main_Bob.java
@@ -0,0 +1,5 @@
+public class Main_Bob {
+    public static void main(String[] args) {
+        System.out.println("bob-b");
+    }
+}
