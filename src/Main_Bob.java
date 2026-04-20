5a5c77c4ca4b6272d95c1e22f531ff03ab81b2c2
marcin-kmp <mduda.kmp@gmail.com>
2026-04-20 19:02:43 +0200

    alice-f

--- src/Main_Bob.java
+++ src/Main_Bob.java
@@ -3,5 +3,6 @@ public class Main_Bob {
         System.out.println("bob-b");
         System.out.println("alice-d");
         System.out.println("alice-e");
+        System.out.println("alice-f");
     }
 }
