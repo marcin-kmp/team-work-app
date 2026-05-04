9c04a45a7f3b491e1f1322a5042ccd2b871dbdba
marcin-kmp <mduda.kmp@gmail.com>
2026-05-04 08:05:24 +0200

    feature-c

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("main-a");
         System.out.println("main-b");
+        System.out.println("feature-c");
     }
 }
---
+++ src/MainFeature.java
@@ -0,0 +1,5 @@
+public class MainFeature {
+    public static void main(String[] args) {
+        System.out.println("feature-c");
+    }
+}
